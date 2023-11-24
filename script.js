55b7759ea21519f077174730726940df6a454aa4
Evgen4X <y.maskaiev.5555@gmail.com>
2023-11-24 21:17:16 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,47 +1,150 @@
-function gen(cols, rows, width) {
-	let html = "";
-	for (let i = 0; i < rows; ++i) {
-		html += `<div class="row" pos="${i}">`;
-		for (let j = 0; j < cols; ++j) {
-			html += `<div class="box" pos="${i}, ${j}" style="width: ${width}; height: ${width}"></div>`;
+class Square {
+	constructor(x, y, width, height, color, id, text = false) {
+		this.x = x;
+		this.y = y;
+		this.w = width;
+		this.h = height;
+		this.color = color;
+		this.id = id;
+		this.text = text;
+	}
+
+	draw(canvas) {
+		canvas.fillStyle = this.color;
+		canvas.fillRect(this.x, this.y, this.w, this.h);
+		if (this.text) {
+			canvas.fillStyle = "rgb(0, 0, 0)";
+			canvas.font = Math.min(this.h, this.w) / 3 + "px serif";
+			canvas.fillText(this.id + 1, this.x + this.w / 5, this.y + this.h / 2);
 		}
-		html += "</div>";
 	}
 
-	board.innerHTML = html;
-}
-
-function start() {
-	combination = [];
-	for (let i = 0; i < level; ++i) {
-		combination.push(`${Math.floor(Math.random() * 5)}, ${Math.floor(Math.random() * 5)}`);
+	collide_point(x, y) {
+		return x >= this.x && x <= this.x + this.w && y >= this.y && y <= this.y + this.h;
 	}
 
-	display();
-}
-
-function pulse(pos, color) {
-	let target = document.querySelector(`.box[pos="${pos}"]`);
-	console.log(target);
-	target.animate(
-		{
-			backgroundColor: color,
-			backgroundColor: "#878787",
-		},
-		{duration: 2000}
-	);
-}
-
-function display() {
-	for (let i = 0; i < level; ++i) {
+	pulse(color, time) {
+		let prev = this.color;
+		this.color = color;
 		setTimeout(() => {
-			pulse(combination[i], "#ff0000");
-		}, i * 2000 + "ms");
+			this.color = prev;
+		}, time);
 	}
 }
 
-const board = document.getElementById("board");
-var level = 1;
-var combination = [];
+function alert_(msg, time) {
+	alert_div.innerHTML = msg;
+	alert_div.style.top = 0;
+	setTimeout(() => {
+		alert_div.style.top = "-20vh";
+	}, time);
+}
 
-gen(5, 5, "10vw");
+function gen(columns, rows) {
+	const width = canvas_size / columns;
+	const height = canvas_size / rows;
+	for (let i = 0; i < rows; ++i) {
+		for (let j = 0; j < columns; ++j) {
+			let square = new Square(10 + j * width, 10 + i * height, width - 20, height - 20, "rgb(100, 100, 100)", i * columns + j, false);
+			squares.push(square);
+		}
+	}
+}
+
+function update() {
+	squares.forEach((square) => {
+		square.draw(ctx);
+	});
+}
+
+function show(restart = false) {
+	if (restart) {
+		combination = [];
+		for (let i = 0; i < combination_length; ++i) {
+			combination.push(Math.floor(Math.random() * squares.length));
+		}
+	} else {
+		combination.push(Math.floor(Math.random() * squares.length));
+	}
+
+	best_score.innerHTML = Math.max(parseInt(best_score.textContent), combination_length);
+
+	combination.forEach((i, i_i) => {
+		setTimeout(() => {
+			squares[i].pulse("rgb(0, 0, 255)", speed);
+		}, i_i * speed * 1.1);
+	});
+
+	setTimeout(() => {
+		listen = true;
+	}, combination_length * speed);
+}
+
+function win() {
+	listen = false;
+	squares.forEach((square) => {
+		if (square.id != combination[combination_length - 1]) {
+			square.color = "rgb(100, 100, 100)";
+			square.pulse("rgb(0, 255, 0)", speed);
+		}
+	});
+	combination_length++;
+	combination_pos = 0;
+}
+
+function lose() {
+	listen = false;
+	alert_(`You lose with score of ${combination_length}!`, 2000);
+	best_score.innerHTML = Math.max(parseInt(best_score.textContent), combination_length);
+	combination_length = 1;
+	combination_pos = 0;
+	combination = [];
+}
+
+function canvas_react(event) {
+	if (!listen) {
+		return;
+	}
+	let x = event.clientX - canvas.getBoundingClientRect().left;
+	let y = event.clientY - canvas.getBoundingClientRect().top;
+	squares.forEach((square) => {
+		if (square.collide_point(x, y)) {
+			if (combination[combination_pos] == square.id) {
+				square.color = "rgb(100, 100, 100)";
+				square.pulse("rgb(0, 255, 0)", speed);
+				combination_pos++;
+				if (combination_pos == combination.length) {
+					win();
+				}
+			} else {
+				square.color = "rgb(100, 100, 100)";
+				square.pulse("rgb(255, 0, 0)", speed);
+				lose();
+			}
+			return;
+		}
+	});
+}
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const canvas_size = canvas.offsetWidth;
+var speed = 200;
+var squares = [];
+var combination = [];
+var combination_length = 1;
+var listen = false;
+var combination_pos = 0;
+
+const speed_input = document.getElementById("speed");
+speed_input.onchange = () => {
+	speed = parseInt(speed_input.value);
+};
+
+const alert_div = document.getElementById("alert");
+const best_score = document.getElementById("best-score-score");
+
+canvas.addEventListener("click", canvas_react);
+
+gen(2, 2);
+setInterval(update, 25);
